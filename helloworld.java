6f86bab587e6e72bc1a8950b6635d7e81a43a571
Reth-Sela <140771589+Reth-Sela@users.noreply.github.com>
2023-12-28 16:25:33 +0700

    add 1 statement in helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -3,5 +3,6 @@ public static void main(String[]args){
 System.out.println("Meyneang I love you so much ");
 System.out.println("And if you love me I promiss I will love you till I die this is my spech can not be change ");
 System.out.println("Do you know that I love you");
+  System.out.println("Yes i know ");
 }
 }

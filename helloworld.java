19ecc9060af10068e705e542fbc1c777df0c985b
Reth-Sela <selareth7@gmail.com>
2023-12-28 16:22:47 +0700

    add helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,7 @@
+public class helloworld{
+public static void main(String[]args){
+System.out.println("Meyneang I love you so much ");
+System.out.println("And if you love me I promiss I will love you till I die this is my spech can not be change ");
+System.out.println("Do you know that I love you");
+}
+}
